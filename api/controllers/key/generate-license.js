a13323c2798e63f1ba997af313824c3966bf6ee0
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2024-03-05 04:42:11 +0100

    logic to generate a number of license keys

---
+++ api/controllers/key/generate-license.js
@@ -0,0 +1,40 @@
+module.exports = {
+  friendlyName: "Generate license",
+
+  description: "",
+
+  inputs: {},
+
+  exits: {},
+
+  fn: async function (inputs) {
+    const { req, res } = this;
+    const { number } = req.params;
+
+    if (!number || number === 0) {
+      return res.badRequest({
+        message:
+          "Invalid Request, Failed to specify number or number set to zero",
+      });
+    }
+
+    function generateKey() {
+      var key = "";
+      var charset = "ABCDEFGHIJKLMONPQRSTUVWXYZ0123456789";
+
+      for (var i = 0; i < 16; i++)
+        key += charset.charAt(Math.floor(Math.random() * charset.length));
+      return key;
+    }
+
+    try {
+      for (let i = 0; i < number; i++) {
+        await License.create({
+          licenseKey: generateKey(),
+        });
+      }
+    } catch (error) {
+      return res.serverError({ message: "Failed to generate license key(s)" });
+    }
+  },
+};
