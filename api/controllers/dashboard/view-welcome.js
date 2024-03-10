5f7c61933149ba75b7453196dc0fd9f6a65da6f3
Timmy Adeyeloja <timmyisadev@gmail.com>
2024-03-10 02:08:54 +0100

    refactored home dashboard

--- api/controllers/dashboard/view-welcome.js
+++ api/controllers/dashboard/view-welcome.js
@@ -1,27 +1,46 @@
 module.exports = {
-
-
-  friendlyName: 'View welcome page',
-
+  friendlyName: "View Dashboard page",
 
   description: 'Display the dashboard "Welcome" page.',
 
-
   exits: {
-
     success: {
-      viewTemplatePath: 'pages/dashboard/welcome',
-      description: 'Display the welcome page for authenticated users.'
+      statusCode: 200,
+      description: "Display the welcome page for authenticated users.",
     },
-
   },
 
-
   fn: async function () {
+    const { res } = this;
 
-    return {};
+    try {
+      const [unactivated, totalNoOfUsers, activated, unactivatedUsers] =
+        await Promise.all([
+          License.find({ keyStatus: "unactivated" }),
+          User.find({ isSuperAdmin: false }),
+          License.find({ keyStatus: "activated" }),
+          User.find({ isSuperAdmin: false, activeSubscription: false }),
+        ]);
 
-  }
+      // Current Active Subscription
+      const subscriptionRecord = await Sub.findOne({
+        owner: this.req.me.id,
+        activeStatus: true,
+      });
 
+      const record = subscriptionRecord || null
 
+      return res.view("pages/dashboard/welcome", {
+        unactivated: unactivated.length,
+        totalNoOfUsers: totalNoOfUsers.length,
+        activated: activated.length,
+        unactivatedUsers: unactivatedUsers.length,
+        currentSubscription:record 
+      });
+    } catch (error) {
+      sails.log.error(error);
+
+      return res.serverError({ message: "Failed" });
+    }
+  },
 };
