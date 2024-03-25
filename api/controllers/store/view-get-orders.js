5cbfa5957e4b426cc4337d689884ea6f42a85a07
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-03-25 19:08:12 +0100

    update: refactor based on user request

--- api/controllers/store/view-get-orders.js
+++ api/controllers/store/view-get-orders.js
@@ -8,10 +8,22 @@ module.exports = {
   fn: async function () {
     const { req, res } = this;
 
-    const orders = await Order.find({ owner: req.me.id });
+    try {
+      if (req.me.isSuperAdmin) {
+        const orders = await Order.find({});
 
-    return res.view("pages/get-orders", {
-      orders,
-    });
+        return res.view("pages/get-orders", {
+          orders,
+        });
+      } else {
+        const orders = await Order.find({ owner: req.me.id });
+        
+        return res.view("pages/get-orders", {
+          orders,
+        });
+      }
+    } catch (error) {
+      return res.serverError();
+    }
   },
 };
