34eddba4d69fda6e90d8ac3f70a446e2f8a9fdb9
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-04-15 20:59:41 +0100

    set up getting review

---
+++ assets/js/pages/review/get-reviews.page.js
@@ -0,0 +1,52 @@
+parasails.registerPage("get-reviews", {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    //…
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function () {
+    //…
+  },
+  mounted: async function () {
+    const form = document.getElementById("form");
+    const review = document.getElementById("review");
+    const productId = document.getElementById("productId");
+
+    async function handleSubmit() {
+      try {
+        const response = await fetch(`/review/${productId.value}`, {
+          method: "POST",
+          body: JSON.stringify(review.value),
+        });
+
+        console.log(response);
+
+        if (response.status === 200) {
+          // window.location.reload()
+        }
+      } catch (error) {
+        console.error(error);
+        alert(
+          "Something went wrong... Could not submit review... Try Again Later"
+        );
+      }
+    }
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      handleSubmit();
+    });
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    //…
+  },
+});
