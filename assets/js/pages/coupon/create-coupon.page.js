78b9cad55c518ea9dd7f0fff99abdbb4dfd6d78d
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2024-04-29 04:44:39 +0100

    chore: implemented coupon cloud logic

---
+++ assets/js/pages/coupon/create-coupon.page.js
@@ -0,0 +1,51 @@
+parasails.registerPage("create-coupon", {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    // Main syncing/loading state for this page.
+    syncing: false,
+    formData: {
+      /* … */
+    },
+
+    // For tracking client-side validation errors in our form.
+    // > Has property set to `true` for each invalid property in `formData`.
+    formErrors: {
+      /* … */
+    },
+
+    // Success state when form has been submitted
+    cloudSuccess: false,
+    // Form Rules
+    formRules: {
+      noOfUsesLeft: { required: true },
+    },
+    // Server error state for the form
+    cloudError: "",
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function () {},
+  mounted: async function () {},
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    createCoupon: async function () {
+      if (this.formData.type === "auto-generated") {
+        this.formData.coupon = "";
+      }
+      
+      this.cloudSuccess = true;
+      window.location.href = "/coupons";
+    },
+
+    deselectCustom: function () {
+      this.formData.coupon = "";
+    },
+  },
+});
